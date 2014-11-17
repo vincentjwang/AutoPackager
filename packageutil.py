1e0762de410eeda694ad1b6149a780a493a52aa4
Vincent.J.Wang <1052114583@qq.com>
2014-11-17 11:05:47 +0800

    modify judge whether template file

--- packageutil.py
+++ packageutil.py
@@ -11,8 +11,30 @@ def getfiles(dirlist):
 
 def generatepackage(dirlist, compilelist):
     oldfilelist = getfiles(dirlist)
+    mockupmsbuild()
+    newfilelist = getfiles(dirlist)
+    
+    #this method need be refactory
+
+    for i in range(0, len(dirlist)):
+        if len(dirlist[i]) == 3:
+            updatedfiles = filegenerator.getupdatedcfiles(oldfilelist[i][0], newfilelist[i][0])
+            if updatedfiles:
+                if not os.path.exists(dirlist[i][1]):
+                    os.makedirs(dirlist[i][1])
+                for f in updatedfiles:
+                    shutil.copy(f, dirlist[i][1])
+        else:
+            updatedfiles = filegenerator.getupdatedtfiles(oldfilelist[i][0], newfilelist[i][0])
+            if updatedfiles:
+                for key, value in updatedfiles.items():
+                    targetpath = dirlist[i][1] + value.replace(dirlist[i][0], '')
+                    if not os.path.exists(targetpath):
+                        os.makedirs(targetpath)
+                    shutil.copy(key, targetpath)
             
-    # mock msbuild compile
+
+def mockupmsbuild():
     f = open(r'D:\VSTS\01_PO\03_Code\01_SourceCode\Silverlight PO Restful\NeweggCentral\ClientBin\NeweggCental.xap.txt', 'w')
     f.write('456')
     f.close()
@@ -30,24 +52,3 @@ def generatepackage(dirlist, compilelist):
     f = open(r'D:\VSTS\01_PO\03_Code\01_SourceCode\Silverlight PO Restful\NeweggCentral.ServiceCenter\Template\SSBTemplate\VendorSSB\v1.txt', 'w')
     f.write('vendor ssb template')
     f.close()
-    
-    newfilelist = getfiles(dirlist)
-
-    #this method need be refactory
-
-    for i in range(0, len(dirlist)):
-        if len(dirlist) > 1 and i == len(dirlist) - 1:
-            updatedfiles = filegenerator.getupdatedtfiles(oldfilelist[i][0], newfilelist[i][0])
-            if updatedfiles:
-                for key, value in updatedfiles.items():
-                    targetpath = dirlist[i][1] + value.replace(dirlist[i][0], '')
-                    if not os.path.exists(targetpath):
-                        os.makedirs(targetpath)
-                    shutil.copy(key, targetpath)
-        else:
-            updatedfiles = filegenerator.getupdatedcfiles(oldfilelist[i][0], newfilelist[i][0])
-            if updatedfiles:
-                if not os.path.exists(dirlist[i][1]):
-                    os.makedirs(dirlist[i][1])
-                for f in updatedfiles:
-                    shutil.copy(f, dirlist[i][1])
